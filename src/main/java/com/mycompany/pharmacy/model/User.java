19d283e6c1c80ce8f91cd0f5ffe3c5048401fbdd
Youssef Mosatafa <youssefmostafa627@gmail.com>
2025-03-25 01:29:13 +0200

    Add basic functions and variables. Add basic function implementations.

--- src/main/java/com/mycompany/pharmacy/model/User.java
+++ src/main/java/com/mycompany/pharmacy/model/User.java
@@ -1,5 +1,49 @@
 package com.mycompany.pharmacy.model;
 
+import com.mycompany.pharmacy.auth.AuthService;
+import java.io.IOException;
+
 public abstract class User {
+    protected String id;
     
+    protected String name;
+    
+    protected String email;
+    
+    protected String phone;
+    
+    protected String password;
+    
+    public void login() throws IOException
+    {
+        AuthService aslogin = new AuthService();
+        
+        aslogin.login(email, password);
+    }
+    
+    public void logout()
+    {
+        AuthService aslogout = new AuthService();
+        
+        aslogout.logout(this);
+    }
+    
+    public void updateProfile(String name, String email, String phone)
+    {
+        this.name = name;
+        
+        this.email = email;
+        
+        this.phone = phone;
+    }
+    
+    public String getEmail()
+    {
+        return email;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
 }
